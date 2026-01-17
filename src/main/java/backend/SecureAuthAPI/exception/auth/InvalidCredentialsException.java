f3389bcbac943bc08b2c4d3ac817b0c3f2bac741
Valentino Castro <jordano7gt@gmail.com>
2026-01-17 01:08:11 -0500

    feat(auth): add InvalidCredentialsException

---
+++ src/main/java/backend/SecureAuthAPI/exception/auth/InvalidCredentialsException.java
@@ -0,0 +1,16 @@
+package backend.secureauthapi.exception.auth;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException() {
+        super("Invalid credentials");
+    }
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+    public InvalidCredentialsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
